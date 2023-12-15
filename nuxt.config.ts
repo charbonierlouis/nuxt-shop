55fce6e5f7cd0a157cb5392df6e5eedcc33bda21
charbonierlouis <louis.charbonier@gmail.com>
2023-12-15 23:00:22 +0100

    build: pre-render products

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
+import type { Product } from './types';
 import { CacheDuration } from './types/cache-duration';
+import axios from 'axios';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -8,6 +10,14 @@ export default defineNuxtConfig({
       apiBase: '',
     },
   },
+  hooks: {
+    async 'nitro:config'(nitroConfig) {
+      const uri = `${process.env.NUXT_PUBLIC_API_BASE}/products`
+      const products = (await axios.get<Product[]>(uri)).data;
+      const slugs = products.map((product) => `/product/${product.id}`);
+      nitroConfig.prerender?.routes?.push(...slugs);
+    },
+  },
   routeRules: {
     '/': {
       swr: CacheDuration.ONE_WEEK,
@@ -15,6 +25,7 @@ export default defineNuxtConfig({
     },
     '/product/:id': {
       swr: CacheDuration.ONE_WEEK,
+      prerender: true,
     },
     '/category/:id': {
       swr: CacheDuration.ONE_WEEK,
